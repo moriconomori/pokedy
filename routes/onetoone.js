6ea326d64bcc557a36dc36c7fa7b36ae194f0e9f
Morico <moriconomori@gmail.com>
2017-02-17 23:03:34 +0900

    Modify one to one question

--- routes/onetoone.js
+++ routes/onetoone.js
@@ -2,11 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+  var attack = getRandomPokemonType();
+  var defense = getRandomPokemonType();
+
   res.render('onetoone', {
-    type: 'onetoone',
-    attack: 'fire',
-    defense: 'water'
+    attack: attack,
+    attackName: typeName[attack],
+    defense: defense,
+    defenseName: typeName[defense]
   });
 });
+
+function getRandomPokemonType() {
+  return pokemonTypes[getRandomInt(0, 17)];
+}
+
+function getRandomInt(min, max) {
+  return Math.floor( Math.random() * (max - min + 1) ) + min;
+}
+
+var pokemonTypes = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"];
+
+var typeName = {
+  Normal: "ノーマル",
+  Fire: "ほのお",
+  Water: "みず",
+  Grass: "くさ",
+  Electric: "でんき",
+  Ice: "こおり",
+  Fighting: "かくとう",
+  Poison: "どく",
+  Ground: "じめん",
+  Flying: "ひこう",
+  Psychic: "エスパー",
+  Bug: "むし",
+  Rock: "いわ",
+  Ghost: "ゴースト",
+  Dragon: "ドラゴン",
+  Dark: "あく",
+  Steel: "はがね",
+  Fairy: "フェアリー"
+}
 
 module.exports = router;
