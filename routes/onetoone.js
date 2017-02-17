77bcc7a630408558267736e81f02ecf9b57b474a
Morico <moriconomori@gmail.com>
2017-02-17 21:59:58 +0900

    Modify favicon

--- routes/onetoone.js
+++ routes/onetoone.js
@@ -2,13 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-
-  res.render('onetoone',
-    {
-      type: 'onetoone',
-      attack: 'fire',
-      defense: 'water'
-    });
+  res.render('onetoone', {
+    type: 'onetoone',
+    attack: 'fire',
+    defense: 'water'
+  });
 });
 
 module.exports = router;
